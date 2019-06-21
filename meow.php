87aa1fa57e7a2a3621810bcc796f13a39916af2b
Rafli Ramadhan <rafliramdhn@gmail.com>
2019-06-21 14:22:38 +0700

    Add feature limit at updated

--- meow.php
+++ meow.php
@@ -25,14 +25,25 @@ function grabbing($url){
 ";
  
 //link
- $link = explode('<h1 class="entry-title title-font"><a href="',$ambilhtml);
+$link = explode('<h1 class="entry-title title-font"><a href="',$ambilhtml);
  //judul
  $judul = explode('alt="',$ambilhtml);
  
- for($x=1;$x<6;$x++){
-    $filterjudul = explode('">',$judul[$x]);
-     $filterlink = explode('" rel="bookmark">', $link[$x]);
+print "masukan limit! (max 12, default 1) :";
+$limit = trim(fgets(STDIN));
 
+if ($limit === "" || (!is_numeric($limit))) {
+	$limit = 1;
+} else {
+	$limit = (int)$limit;
+}
+
+ for($x=1;$x<=$limit;$x++){
+     
+    $filterjudul = explode('">',$judul[$x]);
+    $filterlink = explode('" rel="bookmark">', $link[$x]);
+          
+     
     print "$filterjudul[0]\n";
     print "$filterlink[0]\n\n";
     
